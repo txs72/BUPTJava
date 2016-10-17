0177856de5150a433c01522a473ab138da32fb2f
Tang Xiaosheng <txs@bupt.edu.cn>
2016-10-17 15:21:38 +0800

    added 01/02/03/04 slides' examples

---
+++ slides/01/examples/ShowRuntimeErrors.java
@@ -0,0 +1,6 @@
+// ShowRuntimeErrors.java: Program contains runtime errors
+public class ShowRuntimeErrors {
+  public static void main(String[] args) {
+    System.out.println(1 / 0);
+  }
+}
---
+++ slides/01/examples/ShowSyntaxErrors.java
@@ -0,0 +1,5 @@
+public class ShowSyntaxErrors {
+  public static main(String[] args) {
+    System.out.println("Welcome to Java);
+  }
+}
