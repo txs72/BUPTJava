4522b41871647e721d039bc2901c75f30cf180de
Tang Xiaosheng <txs@bupt.edu.cn>
2016-11-13 22:00:45 +0800

    added material for slides11

---
+++ slides/11/examples/DistinctNumbers.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DistinctNumbers {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+    
+    Scanner input = new Scanner(System.in);   
+    System.out.print("Enter integers (input ends with 0): ");
+    int value;
+    
+    do {
+      value = input.nextInt(); // Read a value from the input
+      
+      if (!list.contains(value) && value != 0) 
+        list.add(value); // Add the value if it is not in the list
+    } while (value != 0);
+
+    // Display the distinct numbers
+    for (int i = 0; i < list.size(); i++) 
+      System.out.print(list.get(i) + " ");
+  }
+}
---
+++ slides/11/examples/DynamicBindingDemo.java
@@ -0,0 +1,38 @@
+public class DynamicBindingDemo {
+  public static void main(String[] args) {
+	Object o = new GraduateStudent(); // implicit casting
+	
+	Object o1 = new Object();
+	
+	if (o1 instanceof GraduateStudent) {
+	  System.out.println("Never got here");
+	  GraduateStudent gs = (GraduateStudent)o1; // explicit casting
+	}
+	
+    m(o);
+    m(new Student());
+    m(new Person());
+    m(new Object());
+  }
+
+  public static void m(Object x) {
+    System.out.println(x.toString());
+  }
+}
+
+class GraduateStudent extends Student {
+}
+
+class Student extends Person {
+  @Override 
+  public String toString() {
+    return "Student";
+  }
+}
+
+class Person extends Object {
+  @Override 
+  public String toString() {
+    return "Person";
+  }
+}
